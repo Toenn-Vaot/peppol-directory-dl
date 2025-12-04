ae348aa897416443a778a61869cb804454c556b0
Yoann BLOSSIER <yoann.blossier@toenn-vaot.fr>
2025-12-05 00:20:16 +0100

    build: initial commit

---
+++ ConsoleApp/Models/PeppolBusinessCard.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ConsoleApp.Models;
+
+public class PeppolBusinessCard
+{
+    [JsonPropertyName("pid")]
+    public string Pid { get; set;}
+
+    [JsonPropertyName("entities")]
+    public IEnumerable<PeppolEntity> Entities { get; set; }
+
+    [JsonPropertyName("docTypes")]
+    public IEnumerable<PeppolDocumentType> DocumentTypes { get; set; }
+}
